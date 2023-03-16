f8a274a3e973bcc54776f69a6d779440f16d1e55
lucas <lucashonorato0093@gmail.com>
2023-03-15 22:26:04 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+tabuada = int(input('Voce é burro, não sabe tabuada. Digite qual tabuada que precisa favor. E não volte mais: '))
+for x in range(1, 11):
+    print('{} * {} = {} '.format(tabuada, x, tabuada * x))
+
